e7aad1ce19d71843d9b32dea3ad548f1665b61ae
chrislacey89 <chrislacey89@gmail.com>
2022-01-21 22:22:56 -0500

    set up check in and checkout

--- app/routes/Houses.tsx
+++ app/routes/Houses.tsx
@@ -1,6 +1,13 @@
+import type { LinksFunction } from 'remix'
 import { Outlet } from 'remix'
 import { AppShell } from '@mantine/core'
 import Header from '../components/Header'
+import housesCSS from '../styles/houses.css'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: housesCSS }]
+}
+
 export default function Houses() {
   return (
     <AppShell
--- app/routes/houses/$houseId.tsx
+++ app/routes/houses/$houseId.tsx
@@ -1,8 +1,17 @@
-import type { LoaderFunction, ActionFunction, MetaFunction } from 'remix'
-import { Link, useLoaderData, useCatch, redirect, useParams } from 'remix'
-import { DateRangePicker } from '@mantine/dates'
+import type { LoaderFunction, MetaFunction } from 'remix'
+import { useLoaderData } from 'remix'
+import { RangeCalendar } from '@mantine/dates'
+import {
+  Title,
+  Input,
+  Popover,
+  Text,
+  Button,
+  Image,
+  InputWrapper,
+} from '@mantine/core'
+import { useState } from 'react'
 import houses from '../../data/houses'
-
 interface House {
   id: string
   picture: string
@@ -24,12 +33,19 @@ export const meta: MetaFunction = ({ data }: { data: House | undefined }) => {
   }
 }
 
-export const loader: LoaderFunction = async ({ request, params }) => {
+export const loader: LoaderFunction = async ({ params }) => {
   return houses.filter(house => house.id === params.houseId)[0]
 }
 
 export default function Houses() {
   const { picture, town, type, title } = useLoaderData<House>()
+  const [value, setValue] = useState<[Date | null, Date | null]>([
+    new Date(2021, 11, 1),
+    new Date(2021, 11, 5),
+  ])
+
+  const [startDateOpened, setStartDateOpened] = useState(false)
+  const [endDateOpened, setEndDateOpened] = useState(false)
 
   return (
     <div className="grid grid-cols-10 gap-8">
@@ -41,7 +57,52 @@ export default function Houses() {
         <p>{title}</p>
       </article>
       <aside className="col-span-4 p-5 border border-solid border-slate-500	">
-        <DateRangePicker></DateRangePicker>
+        <Title order={2}>Choose a Date</Title>
+        <div className="flex flex-col gap-1">
+          <Popover
+            opened={startDateOpened}
+            onClose={() => setStartDateOpened(false)}
+            target={
+              <InputWrapper label="Check In">
+                <Input
+                  onClick={() => setStartDateOpened(o => !o)}
+                  component="button"
+                  value="input"
+                >
+                  {value[0]?.toDateString()}
+                </Input>
+              </InputWrapper>
+            }
+            withArrow
+          >
+            <div style={{ display: 'flex' }}>
+              <RangeCalendar value={value} onChange={setValue} />
+            </div>
+          </Popover>
+          <Popover
+            opened={endDateOpened}
+            onClose={() => setEndDateOpened(false)}
+            target={
+              <InputWrapper label="Check Out">
+                <Input
+                  className="date-input-wrapper"
+                  onClick={() => setEndDateOpened(o => !o)}
+                  component="button"
+                  value="input"
+                >
+                  {value[1]?.toDateString()}
+                </Input>
+              </InputWrapper>
+            }
+            width={260}
+            position="bottom"
+            withArrow
+          >
+            <div>
+              <RangeCalendar value={value} onChange={setValue} />
+            </div>
+          </Popover>
+        </div>
       </aside>
     </div>
   )
