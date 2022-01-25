afd0c2e39c469adfcc0a8ca16ea556e39257be55
chrislacey89 <chrislacey89@gmail.com>
2022-01-24 21:04:36 -0500

    start to set up prisma

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,32 +1,111 @@
+import { useState } from 'react'
 import { Link } from 'remix'
-import { Button, Header as MantineHeader } from '@mantine/core'
+import {
+  Button,
+  Header as MantineHeader,
+  Modal,
+  TextInput,
+  PasswordInput,
+} from '@mantine/core'
+import PasswordStrength from './PasswordStrength'
 export default function Header() {
-  return (
-    <MantineHeader height={80}>
-      <div className="flex">
-        <Link to="/">
-          <a>
-            <img
-              className="h-12 m-4"
-              src="/images/logo-with-text.png"
-              alt="logo"
-            />
-          </a>
-        </Link>
+  const [signUpOpened, setSignUpOpened] = useState(false)
+  const [loginOpened, setLoginOpened] = useState(false)
+  const [email, setEmail] = useState('')
+  const [passwordValue, setPasswordValue] = useState<string>('')
 
-        <nav className="flex items-center	ml-auto">
-          <Link to="/register">
-            <Button variant="subtle" color="dark" size="md" radius="xl">
-              Sign Up
-            </Button>
-          </Link>
-          <Link to="/login">
-            <Button variant="subtle" color="dark" size="md" radius="xl">
+  return (
+    <>
+      <Modal opened={signUpOpened} onClose={() => setSignUpOpened(false)}>
+        <div>
+          <form>
+            <TextInput
+              id="email"
+              type="email"
+              label="Email"
+              placeholder="Email address"
+              value={email}
+              onChange={event => setEmail(event.currentTarget.value)}
+            />
+            <PasswordStrength
+              passwordValue={passwordValue}
+              onPasswordChange={event => {
+                setPasswordValue(event.currentTarget.value)
+              }}
+            />
+            <PasswordInput
+              id="passwordConfirm"
+              type="password"
+              label="Confirm Password"
+              placeholder="Enter your password again"
+            />
+            <Button color="red" fullWidth className="mt-4">
               Log in
             </Button>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal
+        opened={loginOpened}
+        title="Log in"
+        onClose={() => setLoginOpened(false)}
+      >
+        <div>
+          <form>
+            <TextInput
+              id="email"
+              type="email"
+              label="Email"
+              placeholder="Email address"
+            />
+            <PasswordInput
+              id="password"
+              type="password"
+              label="Password"
+              placeholder="Password"
+            />
+            <Button color="red" fullWidth className="mt-4">
+              Log in
+            </Button>
+          </form>
+        </div>
+      </Modal>
+
+      <MantineHeader height={80}>
+        <div className="flex">
+          <Link to="/">
+            <a>
+              <img
+                className="h-12 m-4"
+                src="/images/logo-with-text.png"
+                alt="logo"
+              />
+            </a>
           </Link>
-        </nav>
-      </div>
-    </MantineHeader>
+
+          <nav className="flex items-center	ml-auto">
+            <Button
+              onClick={() => setSignUpOpened(true)}
+              variant="subtle"
+              color="dark"
+              size="md"
+              radius="xl"
+            >
+              Sign Up
+            </Button>
+            <Button
+              onClick={() => setLoginOpened(true)}
+              variant="subtle"
+              color="dark"
+              size="md"
+              radius="xl"
+            >
+              Log in
+            </Button>
+          </nav>
+        </div>
+      </MantineHeader>
+    </>
   )
 }
---
+++ app/components/PasswordStrength.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import { CheckIcon, Cross1Icon } from '@modulz/radix-icons'
+import { Box, PasswordInput, Progress, Text, Popover } from '@mantine/core'
+
+function PasswordRequirement({
+  meets,
+  label,
+}: {
+  meets: boolean
+  label: string
+}) {
+  return (
+    <Text
+      color={meets ? 'teal' : 'red'}
+      sx={{ display: 'flex', alignItems: 'center' }}
+      mt={7}
+      size="sm"
+    >
+      {meets ? <CheckIcon /> : <Cross1Icon />} <Box ml={10}>{label}</Box>
+    </Text>
+  )
+}
+
+const requirements = [
+  { re: /[0-9]/, label: 'Includes number' },
+  { re: /[a-z]/, label: 'Includes lowercase letter' },
+  { re: /[A-Z]/, label: 'Includes uppercase letter' },
+  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'Includes special symbol' },
+]
+
+function getStrength(password: string) {
+  let multiplier = password.length > 5 ? 0 : 1
+
+  requirements.forEach(requirement => {
+    if (!requirement.re.test(password)) {
+      multiplier += 1
+    }
+  })
+
+  return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10)
+}
+
+interface PasswordProps {
+  passwordValue: string
+  onPasswordChange: React.ChangeEventHandler<HTMLInputElement> | undefined
+}
+
+export default function PasswordStrength({
+  passwordValue,
+  onPasswordChange,
+}: PasswordProps) {
+  const [popoverOpened, setPopoverOpened] = useState(false)
+  const checks = requirements.map((requirement, index) => (
+    <PasswordRequirement
+      key={index}
+      label={requirement.label}
+      meets={requirement.re.test(passwordValue)}
+    />
+  ))
+
+  const strength = getStrength(passwordValue)
+  const color = strength === 100 ? 'teal' : strength > 50 ? 'yellow' : 'red'
+
+  return (
+    <Popover
+      opened={popoverOpened}
+      position="bottom"
+      placement="start"
+      withArrow
+      styles={{ popover: { width: '100%' } }}
+      noFocusTrap
+      transition="pop-top-left"
+      onFocusCapture={() => setPopoverOpened(true)}
+      onBlurCapture={() => setPopoverOpened(false)}
+      target={
+        <PasswordInput
+          required
+          label="Your password"
+          placeholder="Your password"
+          description="Strong password should include letters in lower and uppercase, at least 1 number, at least 1 special symbol"
+          value={passwordValue}
+          onChange={onPasswordChange}
+        />
+      }
+    >
+      <Progress
+        color={color}
+        value={strength}
+        size={5}
+        style={{ marginBottom: 10 }}
+      />
+      <PasswordRequirement
+        label="Includes at least 6 characters"
+        meets={passwordValue.length > 5}
+      />
+      {checks}
+    </Popover>
+  )
+}
